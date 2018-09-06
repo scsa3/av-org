4e19bbcb44a2cbabec8271f23bc1353048fbf3dc
Weihan <scsa32@gmail.com>
2018-09-06 18:13:33 +0800

    Clean code

--- avorg/file_handle.py
+++ avorg/file_handle.py
@@ -5,10 +5,12 @@ from typing import Iterable, List
 class FileHandle:
     result: List[Path]
     paths: Iterable[Path]
+    # default_path = r'.'
+    # HACK: this default_path is dev
+    default_path = r'/Users/heweihan/PycharmProjects/av-org/videos'
+    default_extensions = ('.avi', '.mkv', '.mp4')
 
-    def __init__(self, path: str = None):
-        if not path:
-            path = r'.'
+    def __init__(self, path: str = default_path):
         self.directory = Path(path)
         self.get_files_path()
 
@@ -16,34 +18,17 @@ class FileHandle:
         pattern = r'**/*'
         self.paths = list(self.directory.glob(pattern))
 
-    def extension_filter(self, extensions: list = None):
-        if not extensions:
-            extensions = ['.avi', '.mkv', '.mp4']
-        self.result = [path for path in self.paths
+    def extension_filter(self, extensions: list = default_extensions):
+        self.result = [path
+                       for path in self.paths
                        if path.suffix in extensions]
 
     def av_filter(self, patterns: list = None):
-        if not patterns:
-            # TODO: Correct to right behavior.
-            patterns = ['.avi', '.mkv', '.mp4']
-        self.result = [path for path in self.paths
-                       if path.suffix in patterns]
-
-
-def get_files_path(directory: str = r'.') -> list:
-    pattern = r'**/*'
-    paths = Path(directory).glob(pattern)
-    return list(paths)
-
-
-def extension_filter(paths: list = [], extensions: list = []) -> list:
-    return [path for path in paths
-            if path.suffix in extensions]
+        pass
 
 
 if __name__ == '__main__':
     h = FileHandle()
-    h.get_files_path()
     print(h.paths)
     h.extension_filter()
     print(h.result)
