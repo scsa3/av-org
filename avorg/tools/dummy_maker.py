c6c7bea73a5aa997176a2496fd7cfd3101a9a6fe
JohnnyHaHa <scsa32@gmail.com>
2019-04-11 15:06:54 +0800

    Create dummy_maker

---
+++ avorg/tools/dummy_maker.py
@@ -0,0 +1,25 @@
+import sys
+from pathlib import Path
+
+
+def dummy_maker(source_directory: Path, target_directory: Path) -> None:
+    for path in source_directory.glob('**/*'):
+        new_path = target_directory / path
+        if path.is_dir():
+            new_path.mkdir(parents=True, exist_ok=True)
+        elif path.is_file():
+            new_path.touch()
+
+
+if __name__ == '__main__':
+    # arguments by hand
+    source_str = './temp'
+    target_str = './temp2/'
+
+    argv = sys.argv
+    if len(argv) == 3:
+        source_str, target_str = argv[1] = argv[2]
+
+    source = Path(source_str)
+    target = Path(target_str)
+    dummy_maker(source, target)
